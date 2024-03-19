9b2d9f0196d37b330357f4266393181a58fddf7c
Rollins <rshadrackochieng@gmail.com>
2024-03-19 07:40:16 +0300

    portfolio

--- src/app/portfolio/_sections/Experience.jsx
+++ src/app/portfolio/_sections/Experience.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Experience = () => {
   return (
     <div className="py-10">
-      <Container>
+      <Container className="maxwidthContainer">
         <div className="lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
           <div className="lg:col-span-7">
             <div className="grid grid-cols-12 gap-2 sm:gap-6 items-center lg:-translate-x-10">
@@ -31,8 +31,8 @@ const Experience = () => {
                   PERN Fullstack developer - <span className="text-brandBlue">Eastberry Limited</span>
                 </TitleDesc>
                 <p className="text-sm text-gray-600">September, 2023 - January 2024</p>
-                          </div>
-                          
+              </div>
+
               <div className="border-b-2 pb-3">
                 <TitleDesc>
                   Fullstack developer - <span className="text-brandBlue">Vigilux Corporation</span>
